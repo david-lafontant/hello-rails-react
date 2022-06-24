2b664dbb662d475cd3848ae967a28393570c597f
david-lafontant <lafontant.david@gmail.com>
2022-06-24 04:05:53 -0600

    Move file to v1 and Updta routes

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,4 +1,4 @@
-class GreetingsController < ApplicationController
+class V1::GreetingsController < ApplicationController
     def index
         greeting = Greeting.find(rand(1..5))
         render json: greeting, only: [:message]
